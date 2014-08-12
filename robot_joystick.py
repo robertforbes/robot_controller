098c78d81b74e3f5eec88b7c027ab3f000c7d53e
Robert Forbes <robert.forbes@pentlandedge.com>
2014-08-12 22:11:56 +0100

    Added skeleton joystick control.

---
+++ robot_joystick.py
@@ -0,0 +1,40 @@
+import pygame
+    
+pygame.init()
+
+# Clock for polling loop
+clock = pygame.time.Clock()
+
+# Initialise the joysticks
+pygame.joystick.init()
+
+# Find the number of joysticks.
+joystick_count = pygame.joystick.get_count()
+print "joysticks = ", joystick_count
+
+if joystick_count > 0:
+    js = pygame.joystick.Joystick(0)
+    js.init()
+
+    # flag to signal completion
+    complete = False
+
+    # Main game loop
+    while complete == False:
+        
+        for event in pygame.event.get(): # User did something
+            if event.type == pygame.QUIT: # If user clicked close
+                done=True # Flag that we are done so we exit this loop
+            
+            if event.type == pygame.JOYBUTTONDOWN:
+                print("Joystick button down.")
+            if event.type == pygame.JOYBUTTONUP:
+                print("Joystick button up.")
+                
+        # Limit the frame rate to 50Hz.
+        clock.tick(20)
+else:
+    print "No joysticks found."
+
+# Quit on exit.    
+pygame.quit ()
