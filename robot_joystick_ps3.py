d6b2bfa06cfaed17ebf6bb8009490620aa7cfa19
Robert Forbes <robert.forbes@pentlandedge.com>
2014-08-15 00:31:54 +0100

    Identified which axes on the PS3 controller are the ones we are interested in, and mapped these to the appropriate range for transmission to the robot server.

--- robot_joystick_ps3.py
+++ robot_joystick_ps3.py
@@ -34,13 +34,25 @@ if joystick_count > 0:
             
             if event.type == pygame.JOYBUTTONDOWN:
                 print("Joystick button down.")
-                button_state = [js. get_button(x) for x in range(buttons)]
+                button_state = [js.get_button(x) for x in range(buttons)]
                 print button_state
                 
             if event.type == pygame.JOYBUTTONUP:
                 print("Joystick button up.")
-        """
-        if axes >= 2:
+        
+        axis_vals = [js.get_axis(x) for x in range(4)]
+        # print axis_vals
+        # Axis 0 = L/R on left analog control (L = -1.0, R =  1.0)
+        # Axis 1 = U/D on left analog control (D =  1.0, U = -1.0)
+        # Axis 2 = L/R on right analog control
+        # Axis 3 = U/D on right analog control 
+
+        print axis_vals[0], axis_vals[3] 
+        left_right = 127 + round(axis_vals[0] * 127)
+        speed = 127 + round(-axis_vals[3] * 127)
+        print left_right, speed
+        """ 
+	if axes >= 2:
             axis_0 = js.get_axis(0)
             axis_1 = js.get_axis(1)
             print axis_0, axis_1       
